4119a66e69c5a2387ea68437e38527f549aba282
smooth-cat <987444013@qq.com>
2023-04-03 01:57:39 +0800

    fix: 构建&版本发布问题

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import RollupTypescript from 'rollup-plugin-typescript2'
 import pkg from './package.json';
 import NodePath from 'path'
 const resolveFile = path => NodePath.resolve(__dirname, path);
+const bigCamel = (name) => name.split('-').map(it => it[0].toUpperCase() + it.slice(1)).join(''); 
 
 export default [
 	// browser-friendly UMD build
@@ -11,17 +12,17 @@ export default [
 		input: 'src/index.ts',
 		output: {
 			file: pkg.browser,
+      name: bigCamel(pkg.name),
 			format: 'umd'
 		},
 		plugins: [
 			resolve(), // so Rollup can find `ms`
 			commonjs(), // so Rollup can convert `ms` to an ES module
       RollupTypescript({
-        tsconfig: resolveFile('tsconfig.json')
+        tsconfig: resolveFile('tsconfig.rollup.json')
       }),
 		]
 	},
-
 	// CommonJS (for Node) and ES module (for bundlers) build.
 	// (We could have three entries in the configuration array
 	// instead of two, but it's quicker to generate multiple
@@ -30,15 +31,13 @@ export default [
 	// `file` and `format` for each target)
 	{
 		input: 'src/index.ts',
-		external: ['ms'],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
 			{ file: pkg.module, format: 'es' }
 		],
     plugins: [
-      // dts(),
       RollupTypescript({
-        tsconfig: resolveFile('tsconfig.json')
+        tsconfig: resolveFile('tsconfig.rollup.json')
       }),
 		]
 	}
